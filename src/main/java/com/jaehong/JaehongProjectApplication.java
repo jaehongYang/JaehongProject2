a4a6cfa18b17a0e9b185849d15794d11a48b4e07
jaehong.yang <jaehong.yang@bespinglobal.com>
2017-09-27 17:18:37 +0900

    second commit

---
+++ src/main/java/com/jaehong/JaehongProjectApplication.java
@@ -0,0 +1,12 @@
+package com.jaehong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaehongProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaehongProjectApplication.class, args);
+	}
+}
